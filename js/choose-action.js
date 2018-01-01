1fee7de7f80db8cc269508bb3c8dd8373f577995
Victoria <vtr.lung@outlook.es>
2018-01-01 14:05:19 -0500

    subiendo primer y segunda vista

--- js/choose-action.js
+++ js/choose-action.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-  console.log(window.location);
-  // insertando evento que redirecciona la vista de register-phone.html
-  setTimeout(function() {
-    window.location.href = 'register-phone.html';
-  }, 8000);
-});
+// $(document).ready(function() {
+//   console.log(window.location);
+//   // insertando evento que redirecciona la vista de register-phone.html
+//   setTimeout(function() {
+//     window.location.href = 'register-phone.html';
+//   }, 8000);
+// });
